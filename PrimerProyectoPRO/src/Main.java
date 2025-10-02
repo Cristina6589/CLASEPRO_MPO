c0eec6cf9ee7e41eaf2e60925e4754b589c1f498
Cristina6589 <cristinaromesanchez@gmail.com>
2025-10-02 19:50:02 +0200

    update

--- PrimerProyectoPRO/src/Main.java
+++ PrimerProyectoPRO/src/Main.java
@@ -1,3 +1,22 @@
 public class Main {
-    
+
+    public static void main (String[] args) {
+        /*
+        En la versión 24 de jdk siempre te pide al poner main:
+        public static void main (String[] args)
+        */
+        System.out.println("Este es mi primer programa en Java\n");
+        /*
+        Para dar salto de línea o pongo en el println o entre las comillas siempre pongo
+        \n
+         */
+        System.out.println("Este es la segunda línea del programa");
+        //TODO
+        //este tipo de comentario sale resaltado y en color para que lo puedas localizar siempre
+    }
+
+    public void nombreMetodo () {
+
+    }
+
 }
