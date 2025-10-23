97bd259f58d782c61b1541dfbf54570879c2b46e
Cristina6589 <cristinaromesanchez@gmail.com>
2025-10-23 19:50:52 +0200

    update

--- PrimerProyectoPRO/src/Main.java
+++ PrimerProyectoPRO/src/Main.java
@@ -11,9 +11,40 @@ public class Main {
         \n
          */
         System.out.println("Este es la segunda línea del programa\n");
-        System.out.println("Hola bebota");
+        System.out.println("Hola bebota, te cuento un poco sobre mi: \n");
         //TODO
         //este tipo de comentario sale resaltado y en color para que lo puedas localizar siempre
+        String miNombre = "Cristina";
+        /*
+        Esta es una variable de palabra y su valor va definido entre ""
+        (no puedo definir dos varibles con el mismo nombre)
+        y su valor es nulo "null"
+         */
+        char letraDNI = 'N';
+        /*
+        es una variable de letra y su valor va definido por ''
+         */
+        int edad = 36; //es un dato (variable) de número entero sin decimales
+        double altura = 1.70; // número con decimales con precisión (doble de decimales que el float)
+        float peso = 63f; //número con decimales sin precisión (para diferenciar se pone f al final)
+
+        boolean experiencia = false;//true/false sirve para tomar decisiones
+        /*
+        Estas son varialbes mutables, puedo cambiar su valor cuando quiera
+         */
+        final String DNI = "14372024M"; // esta variable sería no mutable, no la puedo cambiar, como el nº PI
+
+        System.out.println("Mi nombre es: "+miNombre);
+        System.out.println("La letra de mi DNI es: "+letraDNI);
+        System.out.println("Mi edad es de: "+edad+" años"); //si vuelvo a poner otro + y " concateno con años
+        System.out.println("Mi altura es de: "+altura+" cm" );
+        System.out.println("Mi peso es de: "+peso+" kg");
+        System.out.println("Mi experiencia es: "+experiencia);
+        System.out.println("Mi DNI es el: "+DNI);
+        System.out.println("El valor del número PI es: "+Math.PI); //ejemplo de no mutable
+
+
+
     }
 
     public void nombreMetodo () {
