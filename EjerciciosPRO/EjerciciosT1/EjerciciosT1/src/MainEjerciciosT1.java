c795ff7fa2afa53736933855c3c76bf6c8b04ce3
Cristina6589 <cristinaromesanchez@gmail.com>
2025-10-27 18:08:19 +0100

    Update

---
+++ EjerciciosPRO/EjerciciosT1/EjerciciosT1/src/MainEjerciciosT1.java
@@ -0,0 +1,51 @@
+public class MainEjerciciosT1 {
+
+    //Ejercicio 1: Definir y mostrar variables
+    public static void main(String[] args) {
+
+        String miNombre = "\nCris";
+        int edad = 36;
+        String miCiudad = "Granada\n";
+
+        System.out.println(miNombre);
+        System.out.println(edad);
+        System.out.println(miCiudad);
+
+    //Ejercicio 2: Modificar variables
+
+
+
+    //Ejercicio 3: Tipos de variables
+
+        String nombre = "Matilda";
+        int edadM = 21;
+        boolean estudios = true;
+        double altura = 1.62;
+        char inicial = 'M';
+
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edadM);
+        System.out.println("¿Es estudiante?: "+estudios);
+        System.out.println("Altura: "+altura);
+        System.out.println("Inicial: "+inicial);
+
+    //Ejercicio 4: Variables con nombres descriptivos
+
+        String titulo = "Don Quijote de la Mancha";
+        String autor = "Miguel de Cervantes";
+        int año = 1605;
+        int paginas = 863;
+        boolean disponibilidad = true;
+
+        System.out.println("\nTítulo: "+titulo);
+        System.out.println("Autor: "+autor);
+        System.out.println("Año de publicación: "+año);
+        System.out.println("Número de páginas: "+paginas);
+        System.out.println("¿Disponible en biblioteca?: "+disponibilidad);
+
+    //Ejercicio 5: Declaración y uso de consonantes
+    }
+
+
+
+}
---
+++ PrimerProyectoMPO/src/Entrada.java
@@ -0,0 +1,8 @@
+public class Entrada {
+
+    //Clase sobre métodos
+
+    public static void main(String[] args) {
+
+    }
+}
